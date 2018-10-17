36dc0cc308779a3935446fad2845a5ef066bacd0
VTC Mobile Jsc <huy.nguyen@yahoo.com>
2018-10-17 15:37:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -86,7 +86,8 @@ def log(msg, *args, **kwargs):  # simple wrapper for logging to stdout on heroku
         if type(msg) is dict:
             msg = json.dumps(msg)
         else:
-            msg = unicode(msg).format(*args, **kwargs)
+            #msg = unicode(msg).format(*args, **kwargs)
+            msg = msg.format(*args, **kwargs)
         print "{}: {}".format(datetime.now(), msg)
     except UnicodeEncodeError:
         pass  # squash logging errors in case of non-ascii text
