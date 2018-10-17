da3e217e472fc9fabf2252f340d2116c4e9bee86
VTC Mobile Jsc <huy.nguyen@yahoo.com>
2018-10-17 14:13:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def webhook():
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]  # the message's text
 
-                    send_message(sender_id, "Chào bạn, bạn vui lòng liên hệ kênh hỗ trợ Scoin của chúng tôi tại địa chỉ http://m.me/scoinvtcmobile hoặc gọi đến tổng đài 19001104 để được hỗ trợ.")
+                    send_message(sender_id, "Chao ban, ban vui long lien he kenh ho tro Scoin cua chung toi tai dia chi m.me/scoinvtcmobile hoac goi den tong dai 19001104 de duoc ho tro.")
 
                 if messaging_event.get("delivery"):  # delivery confirmation
                     pass
