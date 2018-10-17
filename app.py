201c55789b114a99b61c020600f1c5c7d67e248d
VTC Mobile Jsc <huy.nguyen@yahoo.com>
2018-10-17 15:09:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -19,8 +19,8 @@ def verify():
             return "Verification token mismatch", 403
         return request.args["hub.challenge"], 200
 
-sreturn="Chào bạn, bạn vui lòng liên hệ kênh hỗ trợ Scoin của chúng tôi tại địa chỉ m.me/scoinvtcmobile hoặc gọi đến tổng đài 19001104 để được hỗ trợ."
-return sreturn, 200
+sreturn="Chào bạn, bạn vui lòng liên hệ kênh hỗ trợ Scoin của chúng tôi tại địa chỉ m.me/scoinvtcmobile hoặc gọi đến tổng đài 19001104 để được hỗ trợ.".decode('utf-8')
+return sreturn.encode('utf-8'), 200
 
 
 
